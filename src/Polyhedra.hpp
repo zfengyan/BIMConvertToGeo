60156cc9cc5a8c47be70f8587a4363f85ba0e9d2
zmocheng <zmocheng@gmail.com>
2022-03-28 17:14:21 +0200

    Add code to construct polyhedron.

--- src/Polyhedra.hpp
+++ src/Polyhedra.hpp
@@ -20,7 +20,8 @@ struct Polyhedron_builder : public CGAL::Modifier_base<HDS> {
     Polyhedron_builder() {}
     void operator()(HDS& hds) {
         CGAL::Polyhedron_incremental_builder_3<HDS> builder(hds, true);
-        std::cout << "building surface with " << vertices.size() << " vertices and " << faces.size() << " faces" << std::endl;
+        std::cout << "constructing polyhedron -- ";
+        std::cout << "building surface with " << vertices.size() << " vertices and " << faces.size() << " faces" << '\n';
 
         builder.begin_surface(vertices.size(), faces.size());
         for (auto const& vertex : vertices) builder.add_vertex(vertex);
@@ -114,4 +115,87 @@ public:
         std::cout << "construct " << nef.nef_polyhedron_list.size() << " nef polyhedrons " << '\n';
     }
 
+
+    /*
+    * construct polyhedron for each obj shell
+    */
+    static void construct_polyhedron_each_shell_obj(std::string& fname) {
+        std::string path = INTER_PATH;
+        std::string filename = path + fname;
+        std::cout << "-- loading obj shell: " << filename << '\n';
+
+        // read obj
+        std::string line;
+        std::ifstream file(filename);
+        if (!file.is_open()) { std::cerr << "file open failed! " << '\n'; }
+        unsigned long vertex_index = 1; // track vertex index in obj file, starts from 1	   
+        std::vector<double> coordinates; // store xyz coordinates of each vertex line
+        std::vector<unsigned long> face_v_indices; // store face-vertex indices in each face line
+
+        Polyhedron_builder<Polyhedron::HalfedgeDS> polyhedron_builder; // construct polyhedron_builder
+
+        // process each line in the obj file
+        while (std::getline(file, line)) {
+            if (line.empty()) {
+                continue; // skip empty lines:
+            }
+
+            // use markers below to mark the type of each line
+            bool v_flag(false); // entitled "v"
+            bool f_flag(false); // entitled "f"
+
+            // help to process elements in each line
+            std::stringstream ss(line);
+            std::string field; // element in each line
+            std::string::size_type sz; // NOT std::size_t, size of std::string
+
+            // for each element(field, type: string) in one line
+            while (std::getline(ss, field, ' ')) {
+
+                // get the type of current line
+                if (field == "f") {
+                    f_flag = true;
+                    continue;
+                }
+                else if (field == "v") {
+                    v_flag = true;
+                    continue;
+                }
+
+                // process the current line
+                if (v_flag) {
+                    // process xyz coordinates
+                    coordinates.emplace_back(std::stod(field, &sz));
+                }
+                else if (f_flag) {
+                    face_v_indices.emplace_back((unsigned long)std::stoi(field, &sz) - 1);
+                }
+
+            } // end while: process each element in one line
+
+            // process each vertex (if it's a vertex line)
+            if (!coordinates.empty() && coordinates.size() == 3) {
+                polyhedron_builder.vertices.emplace_back(
+                    Point(
+                        coordinates[0], // x
+                        coordinates[1], // y
+                        coordinates[2]) // z
+                );
+                ++vertex_index; // vertex index in obj file
+            }
+            coordinates.clear();
+
+            // constrcut face and add it into faces (if it's a face line)
+            if (!face_v_indices.empty()) {
+                polyhedron_builder.faces.emplace_back(face_v_indices);
+            }
+            face_v_indices.clear();
+
+        } // end while: each line in the file
+
+        // construct polyhedron ------------------------------------------------------------------
+        Polyhedron polyhedron;
+        polyhedron.delegate(polyhedron_builder);
+        std::cout << "done" << '\n';
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,46 @@
 
 int main()
 {
-	std::cout << "Hello CMake." << '\n';
+	//std::cout << "Hello CMake." << '\n';
 	std::cout << "-- activated data folder: " << DATA_PATH << '\n';
 
-	OBJFile f; // organize vertcies, faces, shells and objects
-	
-	std::cout << '\n';
-	std::string fname = "/KIT.obj";
-	LoadOBJ::load_obj(fname, f);
+	//OBJFile f; // organize vertcies, faces, shells and objects
+	//
+	//std::cout << '\n';
+	//std::string fname = "/KIT.obj";
+	//LoadOBJ::load_obj(fname, f);
+
+	//std::cout << '\n';
+	//std::string repeated_info_name = "/KIT.repeated.vertices.txt";
+	//LoadOBJ::repeated_vertices_info(repeated_info_name, f);
+
+	//std::cout << '\n';
+	//std::string repeated_faces_name = "/KIT.repeated.faces.txt";
+	//LoadOBJ::repeated_faces_info(repeated_faces_name, f);
+
+	//std::cout << '\n';
+	//std::string new_vertices_name = "/KIT.new.vertices.txt";
+	//LoadOBJ::process_repeated_vertices(new_vertices_name, f);
+
+	//std::cout << '\n';
+	//std::string new_faces_name = "/KIT.new.faces.txt";
+	//LoadOBJ::process_repeated_faces(new_faces_name, f);
+
+	//std::cout << '\n';
+	//std::string output_obj_name = "/KIT.output.obj";
+	//LoadOBJ::output_obj(output_obj_name, f);
+
+	/* 
+	* prepare the verticesand face - indices for each shell
+	* shell.poly_vertices -- store the vertices of this shell
+	* shell.faces -> face.v_poly_indices -- store the indices(0-based) point to the shell.poly_vertices 
+	*/
+	// PreparePolyhedron::prepare_poly_vertices_face_indices(f); // uncomment this to output each shell
+	// PreparePolyhedron::output_each_shell(f); // uncomment this to output each shell
 
 	std::cout << '\n';
-	std::string repeated_info_name = "/KIT.repeated.vertices.txt";
-	LoadOBJ::repeated_vertices_info(repeated_info_name, f);
-
-	std::cout << '\n';
-	std::string repeated_faces_name = "/KIT.repeated.faces.txt";
-	LoadOBJ::repeated_faces_info(repeated_faces_name, f);
-
-	std::cout << '\n';
-	std::string new_vertices_name = "/KIT.new.vertices.txt";
-	LoadOBJ::process_repeated_vertices(new_vertices_name, f);
-
-	std::cout << '\n';
-	std::string new_faces_name = "/KIT.new.faces.txt";
-	LoadOBJ::process_repeated_faces(new_faces_name, f);
-
-	std::cout << '\n';
-	std::string output_obj_name = "/KIT.output.obj";
-	LoadOBJ::output_obj(output_obj_name, f);
-
-	// prepare the vertices and face-indices for each shell
-	// shell.poly_vertices -- store the vertices of this shell
-	// shell.faces -> face.v_poly_indices -- store the indices(0-based) point to the shell.poly_vertices
-	PreparePolyhedron::prepare_poly_vertices_face_indices(f);
-	PreparePolyhedron::output_each_shell(f); // uncomment this to output each shell
+	std::string obj_name = "/1.obj";
+	Construct_Nef_Polyhedron::construct_polyhedron_each_shell_obj(obj_name);
 	
 	// Nef nef;
 	// Construct_Nef_Polyhedron::construct_nef_polyhedron(f, nef);
